e782ee4ce800dbb37e9b4b1784f78994348ba921
Chris Coetzee <chriscz93@gmail.com>
2020-02-07 11:29:58 +0200

    Apply fix for broken rest-client

--- lib/clickatellsend.rb
+++ lib/clickatellsend.rb
@@ -1,5 +1,6 @@
 require "clickatellsend/version"
 require "rest-client"
+require "clickatellsend/rest_client_response_fix" 
 
 require "clickatellsend/clickatell_config"
 require "clickatellsend/clickatell_request"
